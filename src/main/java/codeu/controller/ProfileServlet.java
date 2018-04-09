4e733c491c305dc885ee7ad27f1d8b49c7c1244b
caseychien <case.chien@gmail.com>
2018-04-09 15:38:53 -0700

    addressed PR comments

--- src/main/java/codeu/controller/ProfileServlet.java
+++ src/main/java/codeu/controller/ProfileServlet.java
@@ -46,22 +46,20 @@ public class ProfileServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    String requestUrl = request.getRequestURI();
-    String username = requestUrl.substring("/profile/".length());
+  String requestUrl = request.getRequestURI();
+  String username = requestUrl.substring("/profile/".length());
+  System.out.println(username); 
 
 	User user = userStore.getUser(username);
     if (user == null) {
-      // couldn't find user
-      request.setAttribute("error", "Sorry, the user" + username + " not found!");
-      request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
-      return;
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
     }
 
     /*get messages from the specific user(NOT DONE)*/
     List<Message> userMessages = new ArrayList<>();
 
     request.setAttribute("user", user);
-    request.setAttribute("messages", usermessages);
+    request.setAttribute("messages", userMessages);
     request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
   }
 
