198ac35080381efd8792fb8f0d1588f630803dd9
Jiaxin Du <jiaxind3@uci.edu>
2018-05-07 20:39:15 -0700

    Add link to profile during conversation

--- src/main/java/codeu/controller/ProfileServlet.java
+++ src/main/java/codeu/controller/ProfileServlet.java
@@ -113,7 +113,6 @@ public class ProfileServlet extends HttpServlet {
     }
 
     User user = userStore.getUser(currentUser);
-    request.setCharacterEncoding("UTF-8");//TODO
     String aboutMe = (String) request.getParameter("aboutMe");
     user.setAboutMe(aboutMe);
 
