c7208cb5c5049cdd985b13014f70576927413ca4
Jiaxin Du <jiaxind3@uci.edu>
2018-05-22 23:41:57 -0700

    fix test

--- src/test/java/codeu/controller/ChatServletTest.java
+++ src/test/java/codeu/controller/ChatServletTest.java
@@ -218,7 +218,7 @@ public class ChatServletTest {
     ArgumentCaptor<Message> messageArgumentCaptor = ArgumentCaptor.forClass(Message.class);
     Mockito.verify(mockMessageStore).addMessage(messageArgumentCaptor.capture());
     Assert.assertEquals(
-        "&#x1F60B;", messageArgumentCaptor.getValue().getContent());
+        "&#x1f60b;", messageArgumentCaptor.getValue().getContent());
 
     Mockito.verify(mockResponse).sendRedirect("/chat/test_conversation");
   }
